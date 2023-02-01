14962591963063470992a4557d6c9065d4ebf08a
Denis Prozorov <den.abraca4@gmail.com>
2023-02-01 18:40:51 +0300

    add solution task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("enter your three digit number");
+int a = int.Parse(Console.ReadLine());
+if (a < 100 || a > 1000)
+{
+   while (a < 100 || a > 999)
+   {
+    System.Console.WriteLine("you entered a wrong number, please enter another");
+    a = int.Parse(Console.ReadLine());
+   } 
+   if (a > 99 && a < 1000)
+   {
+    a = a / 10;
+    a = a % 10;
+    Console.WriteLine(a);
+   } 
+}
+else
+{
+    a = a / 10;
+    a = a % 10;
+    Console.WriteLine(a);
+}
