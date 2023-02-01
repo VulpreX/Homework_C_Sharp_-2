13eabca3b2797fdbea7b65c437d041c1b92a98ae
Denis Prozorov <den.abraca4@gmail.com>
2023-02-01 19:24:49 +0300

    add solution Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter your number");
+long a = long.Parse(Console.ReadLine());
+a = Math.Abs(a);
+if (a > 99)
+{
+    while( a > 999)
+    {
+        a = a / 10;        
+    }
+    a = a % 10;
+    Console.WriteLine(a);
+}
+else 
+{
+    Console.WriteLine("There is no third digit in this number");
+    
+}
+
