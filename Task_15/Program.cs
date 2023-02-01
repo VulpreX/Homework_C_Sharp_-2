c8b18f87fd1ddfc1e376158d4b0aaf9d10a694f2
Denis Prozorov <den.abraca4@gmail.com>
2023-02-01 20:22:48 +0300

    add solution task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] day = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine("enter number 1-7");
+int a = int.Parse(Console.ReadLine());
+a = a - 1;
+if (a < 5 && a >= 0 )
+{
+    System.Console.WriteLine($"{day[a]} is not day of");
+}
+if(a >= 5 && a <= 6)
+{
+    System.Console.Write($"{day[a]} is day of");
+}
+if(a > 6)
+{
+    System.Console.WriteLine(" There is no such day of the week");
+}
